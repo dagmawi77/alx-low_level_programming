7a1cb24ff62df678e5dacc72999304b46392933b
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-09 16:30:57 -0700

    01 to 89

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,16 @@ int main(void)
 {
 	int i,j;
 
-	for (i = 0; i <= 98 ; i++)
+	for (i = '0'; i < '9' ; i++)
 	{
-		for (j = i+1 ;j <= 99 ; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		putchar(' ');
-		putchar((j / 10) + '0');
-		putchar((j % 10) + '0');
-		if (i == 99 && j == 99)
+		if (j != i)
+		{
+			putchar(i);
+			putchar(j);
+		}
+		if (i == '8' && j == '9')
 		{
 		continue;
 		}
