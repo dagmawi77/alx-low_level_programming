cabf70229431ecc938a6962de44b048f7331cac6
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-09 15:42:19 -0700

    base 16 print num and char hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low,i;
+	
+	for(i = '0'; i <= '9'; i++)
+	{
+	 	putchar(i);
+	}
+
+	for (low = 'a'; low <= 'f'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
