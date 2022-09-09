4d8a892b4b5ec15ed54ef89f33fa8c50000a8167
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-09 15:10:12 -0700

    skip e and q charcters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,13 @@
  */
 int main(void)
 {
-	char low , e , q;
-	
+	char low, e, q;
 	e = 'e';
 	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		if( low != e && low != q)
+		if(low != e && low != q)
 		{
 		putchar(low);
 		}
