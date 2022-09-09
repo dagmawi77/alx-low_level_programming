f4158bfb8abc907732d600c6bf7c6e87607fa107
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-09 15:55:31 -0700

    print Number with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9' ; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
