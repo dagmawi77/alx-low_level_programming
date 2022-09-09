92911b1107eb9c49177dc6e7bd1231db65a1f42c
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-09 16:17:03 -0700

    Number from 00 to 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,14 +9,21 @@
  */
 int main(void)
 {
-	int i;
+	int i,j;
 
-	for (i = 0; i <= 99 ; i++)
+	for (i = 0; i <= 98 ; i++)
 	{
+		for (j = i+1 ;j <= 99 ; j++)
+		{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-		if (i != 99)
+		putchar(' ');
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
+		if (i == 99 && j==99)
 		{
+		continue;
+		}
 		putchar(',');
 		putchar(' ');
 		}
