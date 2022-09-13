012601d06f10246e00171ef9f471570eedcd2a4e
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-13 11:28:52 -0700

    a to z fixed final

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 /*
+ *
  *custome header
  */
 
 #include "main.h"
 
 /**
- *  * print_alphabet - prints the alphabet in lowercase.
- *   */
+ * print_alphabet - prints the alphabet in lowercase.  
+ */
 void print_alphabet(void)
 {
 	char low;
