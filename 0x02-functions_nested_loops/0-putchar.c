4fbe6e0055b4017f2ba17e4647e07d6e077a7c7b
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-13 11:10:50 -0700

    _putchar fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 /**
  * main - Entry point
@@ -7,16 +6,16 @@
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-return(0);
+	return(0);
 
 }
