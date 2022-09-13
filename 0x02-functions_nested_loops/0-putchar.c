2c3392ac9a9636598e4b2a82ef8f3f858a1b8921
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-13 10:53:08 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include<studio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-void main()
+int main(void)
 {
-printf("_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
 return(0);
 
 }
