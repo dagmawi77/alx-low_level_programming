205a11626b41fd85fdd5f1be68afeb67ea3fe1b6
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-13 13:23:19 -0700

    sign up

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_sign - prints the alphabet in lowercase.
+ *@n: The char is checked
+ *Return: 1 if character is lowercase 0
+ */
+int print_sign(int n)
+{
+	if (n >0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if ( n==0 )
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
