aec78d8877ee1dfa9c05c5f8e9329cefe7b8bfe4
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-13 11:55:07 -0700

    10 time a-z

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet - prints the alphabet in lowercase.
+ */
+void print_alphabet(void)
+{
+	int count=0;
+	char low;
+	while(count<10)
+	{
+	for (low = 'a'; low <= 'z' ; low++)
+	{
+		_putchar(low);
+	}
+	}
+	_putchar('\n');
+}
