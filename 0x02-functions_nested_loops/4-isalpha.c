0135aacb4a768facad783613dba42143fc5b0ed0
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-13 13:02:19 -0700

    isAlpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isalpha - prints the alphabet in lowercase.
+ *@c: The char is checked
+ *Return: 1 if character is lowercase 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') ||(c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
