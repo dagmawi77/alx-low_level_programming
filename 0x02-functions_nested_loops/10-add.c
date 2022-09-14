957d305fdb9df29e5e9f29991d295ad62dbd9225
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-14 02:12:45 -0700

    Add two Numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  *add - prints the alphabet in lowercase.
  *@num1: the first numm and @ num2 : the second Num
- *@num2:
+ *@num2: the Second number
  *Return: void
  */
 int add(int num1, int num2)
