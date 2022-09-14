41f09f3e7cc2d24e88346b060dc98d5dd668644a
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-14 01:21:17 -0700

    abs value of the n intiger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - prints the alphabet in lowercase.
+ *@n: The char is checked
+ *Return: abs value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
