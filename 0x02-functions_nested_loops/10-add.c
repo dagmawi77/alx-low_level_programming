23ba46569682fcb7e52cf3d8aa12531be62a8139
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-14 02:09:13 -0700

    Add two Numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *times_table - prints the alphabet in lowercase.
+ *@num1: the first numm and @ num2 : the second Num
+ *Return: void
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
