a3d16b08f66845070e53a7a2b83e16288842f9f6
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-14 01:28:21 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_print_last_digit - prints the alphabet in lowercase.
+ *@n: The char is checked
+ *Return: last_d value
+ */
+int  print_last_digit(int n)
+{
+	int last_d = n % 10;
+
+	if (last_d < 0)
+	{
+		last_d *= -1;
+		_putchar(last_d + '0')
+	}
+	return (last_d);
+	
+}
