9a86ecfacf3092a569015f8b85d376fd013b5355
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-14 01:36:08 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,11 @@ int  print_last_digit(int n)
 	int last_d = n % 10;
 
 	if (last_d < 0)
-	{
+	
 		last_d *= -1;
-		_putchar(last_d + '0');
-	}
+
+	_putchar(last_d + '0');
+	
 	return (last_d);
 	
 }
