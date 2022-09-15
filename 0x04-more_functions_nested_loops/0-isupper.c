8b8d444a401d82fea86434bd5c319c6b06d93a21
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-15 13:20:20 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- *_isupper
- *@c:
- *Return:1
+ * _isupper - is function
+ * @c: 
+ * Return:1 if the char is upper ,0 to char is false
  */
+
 int _isupper(int c)
 {
 	if(c >= 'A' && c <= 'Z')
 	{
-	return (1);
+		return (1);
 	}
 	else
 	{
-	return (0);
+		return (0);
 	}
 }
