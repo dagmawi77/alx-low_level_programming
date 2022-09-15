b627b91fc9b9f5672832b5a4022dbd818ef232ac
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-15 14:45:33 -0700

    more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,9 @@ void print_most_numbers(void)
 	{
 		if (i != 2 && i != 4)
 		{
+
 		_putchar((i % 10) + '0');
+		
 		}
 	}
 
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - is multiplication function
+ */
+void more_numbers(void)
+{
+	int i,c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if ( i > 9)
+			{
+			_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
