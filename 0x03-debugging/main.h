1d028252811deb16d28d251dcfd26e3d8c0067d3
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-15 05:30:26 -0700

    Debuging the error

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+/*carrie@ubuntu:/debugging$ cat main.h*/
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
+/*carrie@ubuntu:/debugging$ */
