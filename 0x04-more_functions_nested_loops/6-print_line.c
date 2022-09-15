3608d1dad2d96f0f87653d31f6c02387d7ee59e4
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-15 14:59:31 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - is multiplication function
+ * @n: the number of char
+ */
+void print_line(int n)
+{
+	int l;
+	
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+		{
+		_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}
