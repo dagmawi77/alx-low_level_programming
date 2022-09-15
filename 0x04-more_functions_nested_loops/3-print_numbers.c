0530a85c1cb88ead7fe27f23288d824d3582c7da
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-15 13:43:15 -0700

    print num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - is multiplication function
+ * @void: is the first num
+ * Return: void
+ */
+
+int print_numbers(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		_putchar((n % 10) + '0');
+	}
+	_putchar('\n')
+}
