66a61fec9c07c3e84649dd160a42d9cb62a0d57b
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-16 00:27:04 -0700

    Fix Q6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_line - Draws a straight line using the character _.
+ * print_line - Draws a straight line using the character.
  * @n: The number of _ characters to be printed.
  */
 void print_line(int n)
