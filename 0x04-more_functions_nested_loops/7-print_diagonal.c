13d077e88ae80800b058a70aa1e9376c11f6071d
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-16 00:40:15 -0700

    for Q7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - Draws a straight line using the character.
+ * @n: The number of _ characters to be printed.
+ */
+void print_diagonal(int n)
+{
+	int l, s;
+
+	if (n > 0)
+	{
+		for ( l =0; l < n; l++)
+		{
+			for (s =0; s < l; s++)
+			{
+			_putchar(' ');
+			}
+			_putchar('\\');
+			if(l == n -1)
+			{
+			continue;
+			}
+			_putchar('\n')
+		}
+		
+	}
+
+	_putchar('\n');
+}
