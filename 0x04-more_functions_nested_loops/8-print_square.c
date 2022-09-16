661e8d128808b60f6d136d349326a2d0f5b0b442
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-16 01:03:53 -0700

    Square Question 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,11 +14,11 @@ void print_square(int size)
 			for (w = 0; w < size; w++)
 				_putchar('#');
 
-			if(h == size -1)
+			if (h == size -1)
 				continue;
 			_putchar('\n');
 		}
 	}
 
-	_putchar('\n')
+	_putchar('\n');
 }
