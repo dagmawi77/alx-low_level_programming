98db1c865b77d96acc6047192e5f25c05f3d276e
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-16 01:00:42 -0700

    Square Question 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - Draws a straight line using the character.
+ * @size: The number of _ characters to be printed.
+ */
+void print_square(int size)
+{
+	int h, w;
+
+	if (size > 0)
+	{
+		for (h = 0; w < size; h++)
+		{
+			for (w = 0; w < size; w++)
+				_putchar('#');
+
+			if(h == size -1)
+				continue;
+			_putchar('\')
+		}
+	}
+
+	_putchar('\n')
+}
