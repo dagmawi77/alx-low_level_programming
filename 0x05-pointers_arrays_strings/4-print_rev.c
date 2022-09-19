feca5165bfe41d0fbba89601f96385c0ca31afff
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-19 14:06:51 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,12 @@
  */
 void print_rev(char *str)
 {
-	int l=0,index;
+	int l = 0, index;
 
 	while (str[index++])
 		l++;
 
-	for (index = l -1; index >= 0; index--)
+	for (index = l - 1; index >= 0; index--)
 	       	_putchar(str[index]);
 			
 	_putchar('\n');
