575ad9d2568fbc08e2db9106fa768ad9a0e50b34
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-19 13:48:26 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - is return the length of string
+ * return : len
+ * @str: is pointer one
+ * Return: The length of @str.
+ */
+void _puts(char *s)
+{
+	while (*str++)
+	{
+		putchar(*str++);
+	}
+
+	return ('\n');
+}
