4e48a7b3f3069d841e4abc988a459313681cd148
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-19 14:27:00 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - is return the length of string
+ * return : len
+ * @s: is pointer one
+ * Return: The length of @str.
+ */
+void rev_string(char *str)
+{
+	int len = 0, index;
+	char tmp;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index -1];
+		s[len - index - 1] = tmp;
+	}
+}
