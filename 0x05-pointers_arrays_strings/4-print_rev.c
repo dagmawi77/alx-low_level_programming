c9e20ce40734ca0e3f76c27283c982f8dae56977
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-19 14:02:27 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,10 @@ void print_rev(char *str)
 	int l=0,index;
 
 	while (str[index++])
-	{
 		l++;
+
 	for (index = l -1; index >= 0; index--)
-		        {
-				                _putchar(str[index]);
-						        }
-	}
+	       	_putchar(str[index])
+			
 	_putchar('\n');
 }
