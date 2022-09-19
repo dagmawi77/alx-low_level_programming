2aea5a448c7b09b16b1c11cc669e671006194aa5
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-19 13:31:05 -0700

    swap_int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * reset_to_98 - is return the value
+ * swap_int - is return the value
  * return o
  * @a: is pointer one
  * @b: is pointer two
  */
-void swap_int(int *a, int *b)
+void swap_int(int *a,int *b)
 {
 	int tm = *a;
 	*a = *b;
