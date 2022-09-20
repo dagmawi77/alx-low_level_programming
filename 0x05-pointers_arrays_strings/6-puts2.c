98659e5847078f3099f8d2d8c734e13136be6995
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-20 05:06:30 -0700

    print one char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 print one character
+ * @str: it contain String
+ *
+ */
+
+void puts2(char *str)
+{
+	int index = 0,len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
