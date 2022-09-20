8e0093d215b2cf0ca18dd7c7eb1745057488aeb5
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-20 04:10:40 -0700

    question 4 final

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,18 +2,21 @@
 /**
  * print_rev - is return the length of string
  * return : len
- * @str: is pointer one
+ * @s: is pointer one
  * Return: The length of @str.
  */
-void print_rev(char *str)
+void print_rev(char *s)
 {
 	int len = 0, index;
 
-	while (str[index++])
+	while (s[index++])
+	{
 		len++;
-
+	}
 	for (index = len - 1; index >= 0; index--)
+	{
 		_putchar(str[index]);
+	}
 
 	_putchar('\n');
 }
