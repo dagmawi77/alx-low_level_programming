cf38cfabaa405d76c9d2404ca8966fc5a4a93d24
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-22 13:21:30 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,13 @@
  */
 char *strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i = 0, l = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[ix++])
+		l++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	for (i = 0; src[i]; i++)
+		dest[l++] = src[i];
 
 	return (dest);
 }
