4ade5838214c78811d33e2302058b2815b5474c0
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-22 13:07:30 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - print character
+ * @c: is hold char
+ * Return: is 1.
+ *
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
