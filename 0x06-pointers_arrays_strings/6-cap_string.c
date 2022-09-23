10a1532e8a5e8de0fc94221e14739bde4ce69a69
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-23 14:28:01 -0700

    seven Question

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *str)
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 
-		if(str[i - 1] == ' ' ||
+		if (str[i - 1] == ' ' ||
 		str[i - 1] == '\t' ||
 		str[i - 1] == '\n' ||
 		str[i - 1] == ',' ||
