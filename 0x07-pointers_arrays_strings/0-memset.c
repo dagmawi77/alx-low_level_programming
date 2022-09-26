675e669de1ffee4fcc451d79a2c1419e6edf4b71
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-26 04:44:58 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * memser - Fill the Firest n byetes
+ * @s: A pinter of mem.
+ * @c: The Char to fill mem.
+ * @n: number of byte
+ * Return : The pinter to fill.
+ */
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int i;
+	unsigned char *m = s, v = c;
+
+	for (i = 0; i < n; i++)
+		m[i] = v;
+
+	return (m);
+}
