1533892791cb0fdad25d3ad288ccbac9257f3181
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-28 13:42:52 -0700

    Question 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,46 @@
+#include "main.h"
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+/**
+ * _pow_recursion - print String.
+ * @num: num raised.
+ * @root: power.
+ * Return: l of string
+ */
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+	{
+		return (root);
+	}
+
+	if (root == num / 2)
+	{
+		return (-1);
+	}
+
+	return (find_sqrt(num, root + 1));
+}
+/**
+ * _sqrt_recursion - print String.
+ * @n: num raised.
+ *
+ * Return: l of string
+ */
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 1)
+	{
+		return (1);
+	}
+
+	return (find_sqrt(n, root));
+}
+
