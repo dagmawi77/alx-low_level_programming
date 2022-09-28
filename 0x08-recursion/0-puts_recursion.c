212d078d4c504eda20c046b5f298eb3da5e6a5ef
dagmawi77 <dagmawiletarik@gmail.com>
2022-09-28 13:07:12 -0700

    Question 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - print String.
+ *  @s: printed String.
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+	{
+		_putcahr('\n');
+	}
+}
