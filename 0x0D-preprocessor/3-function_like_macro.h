692bddb748953bdbd49c16c894c39d874171a9ca
Dagmawi Letarik <dagmawiletarik@gmail.com>
2022-10-11 12:09:42 +0300

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-/*
- * Desc: Header file that defines a function-like macro ABS(x)
- */
+
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
