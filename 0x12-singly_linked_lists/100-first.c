f58a371814c738d4806aadcab8ca177f2bde5f6d
Dagmawi Letarik <dagmawiletarik@gmail.com>
2022-10-25 08:54:52 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+/**
+ * hare - Prints
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
