6082868d5cfeb4cb61f52d326e89c326ec87ed93
Dagmawi Letarik <dagmawiletarik@gmail.com>
2022-10-31 21:50:23 +0300

    Create function_pointers.h

---
+++ 0x14-bit_manipulation/function_pointers.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+/*
+ * File: function_pointers.h
+ * AUth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions.
+ */
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
