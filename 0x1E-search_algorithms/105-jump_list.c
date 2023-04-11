65e690c79a2506e5cd58e3bf9fc19f53a0bef41c
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-04-11 16:20:11 +0300

    105-jump_list.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,22 +1,12 @@
-/*
- * File: 105-jump_list.c
- * Auth: Brennan D Baraban
- */
-
 #include "search_algos.h"
 
 /**
- * jump_list - Searches for an algorithm in a sorted singly
- *             linked list of integers using jump search.
+ * jump_list - Searches for an algorithm in a sorted singly.
  * @list: A pointer to the  head of the linked list to search.
  * @size: The number of nodes in the list.
  * @value: The value to search for.
- *
  * Return: If the value is not present or the head of the list is NULL, NULL.
- *         Otherwise, a pointer to the first node where the value is located.
- *
  * Description: Prints a value every time it is compared in the list.
- *              Uses the square root of the list size as the jump step.
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
