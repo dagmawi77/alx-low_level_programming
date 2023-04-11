c42ef7c0a3f44cd4f1983092f7e1685a9fbc74c0
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-04-11 16:07:18 +0300

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,20 +1,11 @@
-/*
- * File: 1-binary.c
- * Auth: Brennan D Baraban
- */
-
 #include "search_algos.h"
 
 /**
-  * binary_search - Searches for a value in a sorted array
-  *                 of integers using binary search.
+  * binary_search - Searches for a value in a sorted array.
   * @array: A pointer to the first element of the array to search.
   * @size: The number of elements in the array.
   * @value: The value to search for.
-  *
   * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the index where the value is located.
-  *
   * Description: Prints the [sub]array being searched after each change.
   */
 int binary_search(int *array, size_t size, int value)
