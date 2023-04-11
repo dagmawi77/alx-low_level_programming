c3b87f82c47d74bdaf295182ae01839397839e42
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-04-11 16:17:01 +0300

    102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,20 +1,10 @@
-/*
- * File: 102-interpolation.c
- * Auth: Brennan D Baraban
- */
-
 #include "search_algos.h"
-
 /**
-  * interpolation_search - Searches for a value in a sorted array
-  *                        of integers using interpolation search.
+  * interpolation_search - Searches for a value in a sorted array.
   * @array: A pointer to the first element of the array to search.
   * @size: The number of elements in the array.
   * @value: The value to search for.
-  *
   * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
-  *
   * Description: Prints a value every time it is compared in the array..
   */
 int interpolation_search(int *array, size_t size, int value)
