0af6065f29195d8b079a9848c74bb1a95eb25a94
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-04-11 16:08:35 +0300

    1-binary_recursive.c

--- 0x1E-search_algorithms/1-binary_recursive.c
+++ 0x1E-search_algorithms/1-binary_recursive.c
@@ -1,21 +1,12 @@
-/*
- * File: 1-binary.c
- * Auth: Brennan D Baraban
- */
-
 #include "search_algos.h"
 
 /**
-  * binary_search_recursive - Searches recursively for a value in a sorted
-  *                           array of integers using binary search.
+  * binary_search_recursive - Searches recursively for a value in a sorted.
   * @array: A pointer to the first element of the [sub]array to search.
   * @left: The starting index of the [sub]array to search.
   * @right: The ending index of the [sub]array to search.
   * @value: The value to search for.
-  *
   * Return: If the value is not present, -1.
-  *         Otherwise, the index where the value is located.
-  *
   * Description: Prints the [sub]array being searched after each change.
   */
 int binary_search_recursive(int *array, size_t left, size_t right, int value)
@@ -39,15 +30,11 @@ int binary_search_recursive(int *array, size_t left, size_t right, int value)
 }
 
 /**
-  * binary_search - Searches for a value in a sorted array
-  *                 of integers using binary search.
+  * binary_search - Searches for a value in a sorted array.
   * @array: A pointer to the first element of the array to search.
   * @size: The number of elements in the array.
   * @value: The value to search for.
-  *
   * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the index where the value is located.
-  *
   * Description: Prints the [sub]array being searched after each change.
   */
 int binary_search(int *array, size_t size, int value)
